586ca9951b756a7825891c4515cb65d54ffff126
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 12:30:43 -0600

    Inicio de programa, solicitud de variables

---
+++ simulador_votacion.py
@@ -0,0 +1,11 @@
+# Simulador votacion
+
+# Solicitud de variables
+
+print("\nBienvenido al menú de votaciones")
+name = str(input("Ingresa tu nombre completo: "))
+dpi = int(input("Ingresa tu número de dpi: "))
+state = str(input("Ingresa el departamento en el cual resides: ")).lower()
+year_of_birth = int(input("Ingresa tu año de nacimiento: "))
+
+# Verificaciones
