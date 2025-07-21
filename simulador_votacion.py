9a639e4536ec970a4965f24a427c9790206af663
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 17:56:11 -0600

    Intento de verificaciones, aplicación de ciclo while true

--- simulador_votacion.py
+++ simulador_votacion.py
@@ -1,11 +1,36 @@
 # Simulador votacion
 
-# Solicitud de variables
+# Menu
+print("\n Bienvenido al menú de votaciones")
 
-print("\nBienvenido al menú de votaciones")
-name = str(input("Ingresa tu nombre completo: "))
-dpi = int(input("Ingresa tu número de dpi: "))
-state = str(input("Ingresa el departamento en el cual resides: ")).lower()
-year_of_birth = int(input("Ingresa tu año de nacimiento: "))
-
+while True:
 # Verificaciones
+
+    # Verificacion name
+    name = str(input("Ingresa tu nombre completo: ")).strip()
+    if len(name) > 5:
+        print("Nombre ingresado correctamente")
+    else:
+        print("El nombre debe de tener más de 5 letras.")
+        break
+
+    # Verificacion dpi
+    dpi = input("Ingresa tu número de dpi: ").strip()
+
+    if len(dpi) == 13:
+        print("El dpi es valido")
+    else:
+        print("El dpi no cuenta con los caracteres suficientes.")
+        break
+
+    #Verificación departamento
+    state = str(input("Ingresa el departamento en el que reside")).lower()
+    if state = "péten" or state = "alta verapaz":
+        special_age = int(input("Ingresa el año en el que naciste: "))
+        if special_age >= 2008:
+            print(f"Bienvenido {name}, su centro de votación esta en {state}")
+        elif special_age < 2008:
+            print("Edad no válida para votar")
+
+
+
