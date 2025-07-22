b577dbbd47480686d870bb78365d661b5d4fffee
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 23:39:18 -0600

    Comienzo de condiciones, realizacion de calculos dependiendo de la condicion

--- clasificador_envio.py
+++ clasificador_envio.py
@@ -9,5 +9,20 @@ size_package = input("Ingresa el tamaño del paquete (pequeño, mediano, grande)
 URGENT = 50
 BIG = 30
 DISCOUNT = 20
+MOUNT = 25
+
+#Urgente, paquete grande y de más 5 kg
+if weight > 5 and urgency == "si" and size_package == "grande":
+    total = weight + URGENT + BIG + MOUNT
+    print("\n Cálculo total:")
+    print(f"El paquete de peso {weight} kg que necesita recorrer una distancia de {distance} km \n Tiene un monto extra de urgencia de Q50 y por tamaño de paquete de Q30 ")
+    print(f"Teniendo un total de {total}")
+# Urgente, paquete no grande y  más de 5 kg
+elif weight > 5 and urgency == "si" and size_package == "pequeño" or size_package == "mediano":
+    total = weight + URGENT + MOUNT
+    print("\n Cálculo total:")
+    print(f"El paquete de peso {weight} kg que necesita recorrer una distancia de {distance} km \n Tiene un monto extra de urgencia de Q50")
+    print(f"Teniendo un total de {total}")
+# Urgente, paquete no grande, menos de 5 kg
 
 
