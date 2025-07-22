e618b6a4b209d2e36b240e42470d19a1c8dfaff7
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 19:24:09 -0600

    Inicio de codigo, solicitud de variables

---
+++ calculadora_impuestos_progresivos.py
@@ -0,0 +1,8 @@
+# Calculadora de impuestos progresivos
+
+# Solicitación de variables
+
+ingreso_anual = float(input("Ingresa el mónto de ingresos anuales en quetzalesa: "))
+
+if ingreso_anual == 0 and ingreso_anual < 30000:
+    dependiente = int(input("Ingresa la cantidad de dependientes: "))
