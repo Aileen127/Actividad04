6484d7b6287eb82b9d01fc260a1e08590bc4f2e4
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 23:24:05 -0600

    Solicitud de datos, definición de constantes y variables

---
+++ clasificador_envio.py
@@ -0,0 +1,13 @@
+
+# Solicitud de datos 
+weight = float(input("Ingrese el peso del paquete en kg: "))
+distance = float(input("Ingrese la distancia en km: "))
+urgency = input("¿El envió es urgente? (si, no): ").lower()
+size_package = input("Ingresa el tamaño del paquete (pequeño, mediano, grande): ").lower()
+
+#Definición constantes
+URGENT = 50
+BIG = 30
+DISCOUNT = 20
+
+
