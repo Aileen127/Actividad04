15a5caf17f1a77ab587a954058d6bdef03213766
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 21:35:19 -0600

    Elaboración ciclo for, condicionales del progama y menú textual

--- sistema_autenticacion_avanzada.py
+++ sistema_autenticacion_avanzada.py
@@ -5,3 +5,17 @@ users = ["Juan", "Maria", "Pedro"]
 # Listas de contraseñas
 passwords = ["Hola123", "flan098", "159num"]
 
+# Ciclo for que cuenta las oportunidades de ingreso
+for attempt in range(3):
+    user = input("Ingresa el usuario por favor: ")
+    if user in users:
+        password = input("Ingresa la contraseña por favor: ")
+        if password in passwords:
+            print("\n Bienvenido al menú")
+            print("1. Ver perfil")
+            print("2. Cambiar contraseña")
+            print("Cerrar sesión")
+        else:
+            print("Acceso denegado")
+    else:
+        print("Acceso denegado.")
