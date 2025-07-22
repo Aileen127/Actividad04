356a9ea14be2c5c88c925e24645484aeb5b36b6f
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 22:05:29 -0600

    Mejora del ciclo, uso de try except para evitar error dentro de consola

--- simulador_facturacion_iva.py
+++ simulador_facturacion_iva.py
@@ -1,11 +1,23 @@
 # Simulador de facturación
 
 # Lista en donde se guarda la info. del precio de productos
-product = []
+products = []
 # Ciclo que se repite hasta que el usuario desee dejar de usarlo
 while True:
-    print("\nBienvenido al simulador de facturación con IVA")
-    price = input("Ingrese el precio del producto: ")
+    # Prevención de error dentro de la consola
+    try:
+        print("\nBienvenido al simulador de facturación con IVA")
+        price = float(input("Ingrese el precio del producto  "))
+        if price > 0:
+            products.append(price)
+        else:
+            print("El precio no debe de ser negativo.")
+    except ValueError:
+        print("Ha ingresado un dato inválido, intente de nuevo.")
+
+    add_more = input("¿Desea agregar más productos? (si, no)").lower()
+    if add_more == "no":
+        break
 
 
 
