899d657884980f6fe9026bda3f0ea1dc5d746593
Aileen127 <fleuraileen394@gmail.com>
2025-07-21 21:51:10 -0600

    Lista y ciclo while True

---
+++ simulador_facturacion_iva.py
@@ -0,0 +1,12 @@
+# Simulador de facturación
+
+# Lista en donde se guarda la info. del precio de productos
+product = []
+# Ciclo que se repite hasta que el usuario desee dejar de usarlo
+while True:
+    print("\nBienvenido al simulador de facturación con IVA")
+    price = input("Ingrese el precio del producto: ")
+
+
+
+
